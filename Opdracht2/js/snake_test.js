1f23419261dd9202d076eda5c651e9a2953ad5e8
Koen Berkhout <koenberkhout@gmail.com>
2019-03-21 19:44:06 +0100

    Basis testbestanden toegevoegd

---
+++ Opdracht2/js/snake_test.js
@@ -0,0 +1,19 @@
+var assert = chai.assert;
+
+describe("Tests voor snake.js", function () {
+
+    //TODO:
+
+    // describe("Funtie fac", function() {
+    //     it("Functie faculteit: ", function () {
+    //         assert.strictEqual(1, fac(0), "0!");
+    //         assert.strictEqual(1, fac(1), "1!");
+    //         assert.strictEqual(7.257415615307994e+306, fac(170), "170!");
+    //     });
+    //     it("Exponent is niet geheel en/of negatief", function () {
+    //         assert.throws(function() { fac(-1); }, Error, "Faculteit kan niet berekend worden");
+    //         assert.throws(function() { fac(1.1);}, Error, "Faculteit kan niet berekend worden");
+    //         assert.throws(function() { fac(171);}, Error, "Faculteit kan niet berekend worden");
+    //     });
+    // });
+});
