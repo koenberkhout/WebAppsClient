79d3f3904d30316dbb693a26f5f8f2165004a680
Koen Berkhout <koenberkhout@gmail.com>
2019-03-22 13:49:36 +0100

    Test toegevoegd voor collidesWithOneOf()

--- Opdracht2/js/snake_test.js
+++ Opdracht2/js/snake_test.js
@@ -1,19 +1,19 @@
 var assert = chai.assert;
 
 describe("Tests voor snake.js", function () {
-
-    //TODO:
-
-    // describe("Funtie fac", function() {
-    //     it("Functie faculteit: ", function () {
-    //         assert.strictEqual(1, fac(0), "0!");
-    //         assert.strictEqual(1, fac(1), "1!");
-    //         assert.strictEqual(7.257415615307994e+306, fac(170), "170!");
-    //     });
-    //     it("Exponent is niet geheel en/of negatief", function () {
-    //         assert.throws(function() { fac(-1); }, Error, "Faculteit kan niet berekend worden");
-    //         assert.throws(function() { fac(1.1);}, Error, "Faculteit kan niet berekend worden");
-    //         assert.throws(function() { fac(171);}, Error, "Faculteit kan niet berekend worden");
-    //     });
-    // });
+    describe("Functie collidesWithOneOf", function () {
+        var myElements = [
+            createSegment(40, 40),
+            createSegment(40, 60),
+            createSegment(40, 80)
+        ];
+        it("'Dit' element niet in elements", function () {
+            var myElement = createSegment(100, 100);
+            assert.isFalse(myElement.collidesWithOneOf(myElements));
+        });
+        it("'Dit' element wel in elements", function () {
+            var myElement = createSegment(40, 60);
+            assert.isTrue(myElement.collidesWithOneOf(myElements));
+        });
+    });
 });
