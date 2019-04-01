a2de30917b1d628d4303a7d6d3e9de151d40a802
Koen Berkhout <koenberkhout@gmail.com>
2019-04-01 18:50:32 +0200

    cloned 2 as base for 3

---
+++ Opdracht3/js/snake_test.js
@@ -0,0 +1,24 @@
+var assert = chai.assert;
+
+describe("Tests voor snake.js", function () {
+    describe("Functie collidesWithOneOf()", function () {
+        var myElements = [
+            createSegment(40, 40),
+            createSegment(40, 60),
+            createSegment(40, 80)
+        ];
+        it("'Dit' element niet in elements", function () {
+            var myElement = createSegment(100, 100);
+            assert.isFalse(myElement.collidesWithOneOf(myElements));
+        });
+        it("'Dit' element wel in elements", function () {
+            var myElement = createSegment(40, 60);
+            assert.isTrue(myElement.collidesWithOneOf(myElements));
+        });
+        it("Test met een lege array", function () {
+            var emptyArray = [];
+            var myElement = createSegment(40, 60);
+            assert.isFalse(myElement.collidesWithOneOf(emptyArray));
+        });
+    });
+});
