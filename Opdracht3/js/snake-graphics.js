87e027bb76eb43e56f89a0301d3bff7337fdbe90
Koen Berkhout <koenberkhout@gmail.com>
2019-04-02 21:45:28 +0200

    documentatie toegevoegd voor snake-graphics.js

--- Opdracht3/js/snake-graphics.js
+++ Opdracht3/js/snake-graphics.js
@@ -43,14 +43,28 @@ var graphics = (function() {
         });
     }
 
+    /**
+     * @function showWon
+     * @description Geeft een overwinningstekst in het groen weer op het canvas
+     */
     function showWon() {
         writeText("Gewonnen!", "#35a74a")
     }
 
+    /**
+     * @function showLost
+     * @description Geeft een verliezingstekst in het rood weer op het canvas
+     */
     function showLost() {
         writeText("Verloren!", "#ff0000")
     }
 
+    /**
+     * @function writeText
+     * @description Geeft een bepaalde tekst in een bepaalde kleur weer op het canvas
+     * @param {string} text de tekst
+     * @param {string} color kleur van de tekst
+     */
     function writeText(text, color) {
         clear();
 
@@ -67,10 +81,21 @@ var graphics = (function() {
         });
     }
 
+    /**
+     * @function clear
+     * @description Maakt het canvas leeg
+     */
     function clear() {
         canvas.clearCanvas();
     }
 
+    /**
+     * @function showProgress
+     * @description Toont een progress bar aan de bovenkant van het scherm (topbar),
+     * een halftransparante overlay en wijzigt de knop waarop is gedrukt in twee
+     * ronddraaiende pijltjes.
+     * @param {string} action Begin van het id van de actieknop "save.." of "load.."
+     */
     function showProgress(action) {
         topbar.show();
         $("#overlay").fadeIn(200);
@@ -78,6 +103,12 @@ var graphics = (function() {
         $(action + "SnakeIndicator").show();
     }
 
+    /**
+     * @function hideProgress
+     * @description Verbergt de progress-elementen die eerder met showProgress op het scherm
+     * waren gezet, en doet dit met een lichte vertraging :)
+     * @param {string} action Begin van het id van de actieknop "save.." of "load.."
+     */
     function hideProgress(action) {
         setTimeout(function() {
             topbar.hide();
@@ -87,6 +118,14 @@ var graphics = (function() {
         }, 250);
     }
 
+    /**
+     * @function updateStats
+     * @description Toont de statistieken (overwinningen/verliezen) in de tabel onder het spel.
+     * @param {number} wonNow Aantal overwinningen deze sessie
+     * @param {number} lostNow Aantal verliezen deze sessie
+     * @param {number} wonEver Aantal overwinningen totaal (van server)
+     * @param {number} lostEver Aantal verliezen totaal (van server)
+     */
     function updateStats(wonNow, lostNow, wonEver, lostEver) {
         $("#wonNow").text(wonNow);
         $("#lostNow").text(lostNow);
