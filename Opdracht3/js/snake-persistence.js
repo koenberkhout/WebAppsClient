615a4cb4605de358cdff7e4dcdbbbe3b6f84acd8
Koen Berkhout <koenberkhout@gmail.com>
2019-04-02 21:51:33 +0200

    documentatie toegevoegd voor snake-persistence.js

--- Opdracht3/js/snake-persistence.js
+++ Opdracht3/js/snake-persistence.js
@@ -8,6 +8,12 @@ var persistence = (function() {
     var lostEver   = 0;
 
 
+    /**
+     * @function save
+     * @description Stuurt het aantal overwinningen en verliezen die in de huidige
+     * sessie zijn gemaakt naar de server in json-formaat. Het resultaat wordt opgeslagen
+     * in wonEver en lostEver. Het opdaten op het scherm wordt gedelegeerd aan 'graphics'.
+     */
     function save() {
         graphics.showProgress("#save");
 
@@ -33,6 +39,11 @@ var persistence = (function() {
         })
     }
 
+    /**
+     * @function load
+     * @description Haalt de spelstatistieken op van de server (totaal aantal overwinningen
+     * en verliezen) en delegeert het updaten van de weergave hiervan aan module 'grapics'.
+     */
     function load() {
         graphics.showProgress("#load");
 
@@ -53,16 +64,31 @@ var persistence = (function() {
         });
     }
 
+    /**
+     * @function addWon
+     * @description Verhoogt het aantal overwinningen van deze sessie met 1 en
+     * delegeert het updaten van de weergave hiervan aan module 'grapics'.
+     */
     function addWon() {
         won++;
         updateStats();
     }
 
+    /**
+     * @function addLost
+     * @description Verhoogt het aantal verliezen van deze sessie met 1 en
+     * delegeert het updaten van de weergave hiervan aan module 'grapics'.
+     */
     function addLost() {
         lost++;
         updateStats();
     }
 
+    /**
+     * @function updateStats
+     * @description Geeft de huidige statistieken mee aan de graphics-module
+     * om deze te kunnen opdaten op het scherm.
+     */
     function updateStats() {
         graphics.updateStats(won, lost, wonEver, lostEver);
     }
